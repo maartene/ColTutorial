32289c2f83f6b0cf3788e9821ee549da8db9eec4
Maarten Engels <maartene@thedreamweb.eu>
2024-08-02 14:13:56 +0200

    episode 05

--- ColTutorial/ContentView.swift
+++ ColTutorial/ContentView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 import SpriteKit
 
 struct ContentView: View {
+    let scene = GameScene(size: CGSize(width: 375, height: 667))
     var body: some View {
-        SpriteView(scene: GameScene(size: CGSize(width: 375, height: 667)), options: [.ignoresSiblingOrder], debugOptions: [.showsDrawCount, .showsFPS, .showsNodeCount])
-            .ignoresSafeArea()
+        ZStack {
+            SpriteView(scene: scene, options: [.ignoresSiblingOrder], debugOptions: [.showsDrawCount, .showsFPS, .showsNodeCount])
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        scene.left()
+                    } label: {
+                        Image(systemName: "arrowshape.left.fill")
+                        Text("Left  ")
+                    }
+                    .foregroundColor(.accentColor)
+                    .padding()
+                    .background(Color.secondary)
+                    .clipShape(.capsule)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Button {
+                        scene.right()
+                    } label: {
+                        Text("Right")
+                        Image(systemName: "arrowshape.right.fill")
+                    }
+                    
+                    .foregroundColor(.accentColor)
+                    .padding()
+                    .background(Color.secondary)
+                    .clipShape(.capsule)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                }
+                .padding()
+                .font(.largeTitle)
+                Text("").padding()
+            }
+            
+        }
     }
 }
 
